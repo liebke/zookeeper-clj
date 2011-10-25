31ed3940b26f0aeb2068bfae5b00aad130299b2f
David Edgar Liebke <liebke@gmail.com>
2011-10-24 21:44:50 -0400

    Added zookeeper.server namespace again.

---
+++ src/zookeeper/server.clj
@@ -0,0 +1,13 @@
+(ns zookeeper.server
+  (:import (org.apache.zookeeper.server ZooKeeperServerMain
+                                        ServerConfig)))
+
+(defn server-config
+  ([filename]
+     (-> (ServerConfig.)
+         (.parse filename))))
+
+(defn start-server
+  ([config-filename]
+     (-> (ZooKeeperServerMain.)
+         (.runFromConfig (server-config config-filename)))))
