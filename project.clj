8fe8f5c9a00c84f4c213f40d13f4514187cb6a4e
David Edgar Liebke <liebke@gmail.com>
2013-04-09 09:45:56 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject zookeeper-clj "0.9.2"
+(defproject zookeeper-clj "0.9.3"
   :description "A Clojure DSL for Apache ZooKeeper"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.zookeeper/zookeeper "3.3.2"]
-                 [log4j/log4j "1.2.16"]
-                 [commons-codec "1.5"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.zookeeper/zookeeper "3.4.0"]
+                 [log4j/log4j "1.2.17"]
+                 [commons-codec "1.7"]])
