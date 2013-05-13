17d920f5ac89a1a32a7afd0557037939c2d91777
Fraz <Fraz@mailonline.co.uk>
2013-05-13 17:10:12 +0100

    new project file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zookeeper-clj "0.9.3"
+(defproject zookeeper-clj "0.9.4-timeout-fix"
   :description "A Clojure DSL for Apache ZooKeeper"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.zookeeper/zookeeper "3.4.0"]
