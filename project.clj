642615f02dea7f6f4797b6ea72f820a421857572
David Cuddeback <david.cuddeback@gmail.com>
2013-06-13 12:17:52 -0700

    excludes slf4j binding from compile-time dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zookeeper-clj "0.9.3"
   :description "A Clojure DSL for Apache ZooKeeper"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.zookeeper/zookeeper "3.4.0"]
+                 [org.apache.zookeeper/zookeeper "3.4.0" :exclusions [org.slf4j/slf4j-log4j12]]
                  [log4j/log4j "1.2.17"]
                  [commons-codec "1.7"]])
