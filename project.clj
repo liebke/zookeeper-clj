edd79671667e836bad3c6f072050ccba2d071f5d
ben.sless <ben.sless@appsflyer.com>
2020-07-16 20:26:04 +0300

    [#28] Update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject zookeeper-clj "0.9.5-SNAPSHOT"
   :description "A Clojure DSL for Apache ZooKeeper"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.zookeeper/zookeeper "3.4.6"]
-                 [commons-codec "1.7"]
-                 [org.apache.curator/curator-test "2.8.0" :scope "test"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.apache.zookeeper/zookeeper "3.6.1"]
+                 [commons-codec "1.14"]
+                 [org.apache.curator/curator-test "5.1.0" :scope "test"]]
   :repositories {"clojars" {:sign-releases false :url "https://clojars.org/repo/"}}
-  :plugins [[lein-ancient "0.6.8"]
-            [lein-cljfmt "0.3.0"]])
+  :plugins [[lein-ancient "0.6.15"]
+            [lein-cljfmt "0.6.8"]])
